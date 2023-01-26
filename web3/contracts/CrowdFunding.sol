5b53357b0fe15528d4dfc63c2c0d9afaf7609d4f
MohammadSheakh <mohammad.sheakh@gmail.com>
2023-01-26 16:19:09 +0600

    back-end

--- web3/contracts/CrowdFunding.sol
+++ web3/contracts/CrowdFunding.sol
@@ -1,60 +1,89 @@
 // SPDX-License-Identifier: UNLICENSED
+// this is solidity file..
 pragma solidity ^0.8.9;
 
 contract CrowdFunding {
+    // acts as object in js
     struct Campaign {
         address owner;
         string title;
         string description;
-        uint256 target;
+        uint256 target; // uint256 means number
         uint256 deadline;
         uint256 amountCollected;
-        string image;
-        address[] donators;
-        uint256[] donations;
+        string image; // image er url save korbo 
+        address[] donators; // array of addresss
+        uint256[] donations; // array of amount of donations 
     }
 
-    mapping(uint256 => Campaign) public campaigns;
+    mapping(uint256 => Campaign) public campaigns; // bujhi nai 😥
 
-    uint256 public numberOfCampaigns = 0;
+    uint256 public numberOfCampaigns = 0; // koto gula campaign create korlam .. sheta track rakhbo
 
-    function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image) public returns (uint256) {
-        Campaign storage campaign = campaigns[numberOfCampaigns];
+    function createCampaign(
+        address _owner, 
+        string memory _title, // string er shathe memory jinish ta lage .. 
+        string memory _description, 
+        uint256 _target, 
+        uint256 _deadline, 
+        string memory _image
+    ) public returns (uint256) {
+        /**
+         * in solidity .. you have to specify if this function is only internal or if you can use it from our front-end.. so
+         * since this one will be public .. we can add a public keyword right here.. and then we even have to specify what is 
+         * going to return .. sheta amra () er modhdhe bole dibo . we return the id of that specific campaign
+         */
+        Campaign storage campaign = campaigns[numberOfCampaigns]; // create new Campaign
+        // first e numberOfCampaign hocche 0 .. pore amra increase korbo 
 
+        // require statement in solidity is like a check .. to see everything is good .. 
+        // is everything ok ? 
+        // block.timestamp hocche current time ..campaign er deadline,  current time er cheye choto hoile error dekhabe je 
+        // campaign er deadline er time current time theke beshi hoite hobe .. 
         require(campaign.deadline < block.timestamp, "The deadline should be a date in the future.");
+        // eta satisfy na hoile .. code shamne agabe na .. 
 
         campaign.owner = _owner;
         campaign.title = _title;
         campaign.description = _description;
         campaign.target = _target;
         campaign.deadline = _deadline;
-        campaign.amountCollected = 0;
+        campaign.amountCollected = 0; // create houar shomoy 0 .. pore donate korle increase hobe .. 
         campaign.image = _image;
 
         numberOfCampaigns++;
 
-        return numberOfCampaigns - 1;
+        return numberOfCampaigns - 1; // index return korlam .. most newly created campaign
     }
 
     function donateToCampaign(uint256 _id) public payable {
-        uint256 amount = msg.value;
+        // we need to get the id of the campaign we want to donate the money to and its going to be public and 
+        // its going to be payable .. its a special keyword that signifies that we are going to send some 
+        // cryptocurrency through out the function .. 
+        uint256 amount = msg.value; // this is what we we are trying to send from our front-end
 
-        Campaign storage campaign = campaigns[_id];
+        // we are then going to get the campaign we want to donate to
+        Campaign storage campaign = campaigns[_id]; // campaigns ta hocche maping .. jeta amra shuru te create korsilam .. 
 
-        campaign.donators.push(msg.sender);
+        campaign.donators.push(msg.sender); // donator er address push kortesi jeta front-end theke ashse 
         campaign.donations.push(amount);
 
-        (bool sent,) = payable(campaign.owner).call{value: amount}("");
+        // send hocche variable .. eta amader janabe actually send hoise kina 
+        (bool sent, ) = payable(campaign.owner).call{value: amount}("");      
+        // amra campaign er owner er kase pathacchi .. 
 
         if(sent) {
-            campaign.amountCollected = campaign.amountCollected + amount;
+            campaign.amountCollected = campaign.amountCollected + amount; // ager value er shathe new value add kore dilam 
         }
     }
 
+    // list of all dotator who donate a compaign
     function getDonators(uint256 _id) view public returns (address[] memory, uint256[] memory) {
+        // we need to know of which campaign do we want to get the donators form
         return (campaigns[_id].donators, campaigns[_id].donations);
     }
 
+    // list of all campaigns
     function getCampaigns() public view returns (Campaign[] memory) {
         Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
 
